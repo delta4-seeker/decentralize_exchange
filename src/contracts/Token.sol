775250677ed99c86247c127753954b1ea2bd8192
detla4-seeker <9165prashant@gmail.com>
2022-11-20 17:57:30 +0545

    tested balanceOf and trasfer method

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -1,6 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
-
+import "openzeppelin-solidity/contracts/utils/math/SafeMath.sol" ; 
 contract Token{
-    string public name= "MY name" ; 
+    string public name= "Brij Token" ; 
+    string public symbol = "BRJ" ; 
+    uint256 public decimal = 10 ;
+    uint256 public totalSupply ;
+
+    event Transfer(address indexed from , address indexed to , uint256 value);
+
+    //track balance
+    //send token 
+    mapping(address => uint256) public balanceOf ; 
+    constructor()  {
+        totalSupply = 1000000 * (10**decimal) ; 
+        balanceOf[msg.sender] = totalSupply ; 
+
+    }
+
+    function transfer(address _to , uint256 _value) public returns (bool success) { 
+        require(balanceOf[msg.sender] >= _value);
+        balanceOf[msg.sender] = balanceOf[msg.sender] - _value ; 
+        balanceOf[_to]  = balanceOf[_to] + _value ; 
+        emit Transfer(msg.sender , _to , _value);
+        return true ; 
+    }
+
 }
---
+++ test/Token.test.js
@@ -0,0 +1,81 @@
+
+const Token = artifacts.require('./Token')
+
+require('chai').use(require('chai-as-promised')).should()
+
+
+contract('Token' ,  ([deployer, receiver ]) => {
+  const  name = "Brij Token" ; 
+  const symbol = "BRJ" ; 
+  const decimal = 10 ; 
+  const totalSupply = '10000000000000000' ; 
+  let token ; 
+
+  beforeEach( async () => {
+    token = await Token.new();
+  })
+
+  describe('deployment' , () => {
+    it('tracks the name' , async () => {
+      const result = await token.name() 
+      result.should.equal( name)
+    })
+
+    it('tracks the symbol' , async () => {
+      const result = await token.symbol() ; 
+      result.should.equal(symbol);
+    })
+    it('should track total sypply' , async () => {
+      const result =  await token.totalSupply();
+      result.toString().should.equal(totalSupply);
+    })
+
+    it(' should assign the total supply to the deployer' , async () => {
+      const total = await token.totalSupply() ; 
+      const result = await token.balanceOf(deployer) ; 
+      result.toString().should.equal(total.toString()) ; 
+    })
+  })
+
+  describe('sending tokens ' , () => {
+    let result ; 
+
+    describe('success' , () => {
+      beforeEach( async () => {
+        result = await  token.transfer(receiver , "100000000000000" , { from : deployer})
+       })
+       it('transfer token to receipient' ,async () => {
+         let balanceOf ; 
+         balanceOf = await token.balanceOf(receiver) ;
+         balanceOf.toString().should.equal("100000000000000")
+         balanceOf = await token.balanceOf(deployer) ; 
+         balanceOf.toString().should.equal((parseInt(totalSupply) - parseInt("100000000000000")).toString());
+   
+       })
+   
+   
+       it('emit a trasfer event ' ,  () => {
+         result.logs[0].event.should.equal('Transfer');
+         let event = result.logs[0].args
+         event.to.toString().should.equal(receiver);
+         event.from.toString().should.equal(deployer);
+         event.value.toString().should.equal("100000000000000")
+       
+       })
+   
+    })
+ 
+    describe('failure' , () => {
+
+      it('rejects insufficient balance ' , async () => {
+        let invalidAmount = "100000000000000000000000000000000" ; 
+        await token.transfer(receiver , invalidAmount , {from: deployer}).should.be.rejectedWith('VM Exception while processing transaction: revert');
+
+      }) 
+
+    })
+ 
+
+  })
+
+})
